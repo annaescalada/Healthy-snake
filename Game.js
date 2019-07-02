f44d42dc585160b5d328a726c89fdaebbf4ba288
annaescalada <annaescalada@gmail.com>
2019-07-02 15:44:34 +0200

    Obstacles improved

--- Game.js
+++ Game.js
@@ -8,19 +8,18 @@ function Game(canvas) {
   this.onGameOver = null;
   this.canvas = canvas;
   this.ctx = this.canvas.getContext('2d');
+  this.totalScore = 0;
   this.level = 1;
 }
 
 Game.prototype.startGame = function () {
 
   this.snake = new Snake(this.canvas);
+
   var canvasWithByObjectSize = this.canvas.width / this.snake.width;
   var randomX = this.snake.width * (Math.floor(Math.random() * (canvasWithByObjectSize)));
   var randomY = this.snake.width * (Math.floor(Math.random() * (canvasWithByObjectSize)));
-  
-  this.food = new Food(this.canvas,randomX,randomY);
-
-  //create new food: random x random y this.food = Food (randomx, randomy)
+  this.food = new Food(this.canvas, randomX, randomY);
 
   var counter = 0;
   var loop = () => {
@@ -29,19 +28,21 @@ Game.prototype.startGame = function () {
       var canvasWithByObjectSize = this.canvas.width / this.snake.width;
       var randomX = this.snake.width * (Math.floor(Math.random() * (canvasWithByObjectSize)));
       var randomY = this.snake.width * (Math.floor(Math.random() * (canvasWithByObjectSize)));
-      this.food = new Food(this.canvas,randomX,randomY);
+      this.food = new Food(this.canvas, randomX, randomY);
+      this.newFood = false;
     }
- 
+
     if (counter === 60) {
       counter = 0;
 
       this.update();
       this.clear();
       this.draw();
-//    this.checkCollisions();
+      this.score();
+      //    this.checkCollisions();
     }
 
-    if(!this.isGameOver) {
+    if (!this.isGameOver) {
       requestAnimationFrame(loop);
     } else {
       this.onGameOver();
@@ -66,9 +67,17 @@ Game.prototype.draw = function () {
   // })
 }
 
-// Game.prototypeScore {
+Game.prototype.score = function () {
+  var score = (this.snake.positions[0].x === this.food.x && this.snake.positions[0].y === this.food.y);
 
-// }
+  if (score) {
+    this.totalScore = this.totalScore + 10;
+    this.newFood = true;
+  }
+
+  console.log(this.totalScore);
+
+}
 
 
 // Game.prototype.checkCollisions = function () {
